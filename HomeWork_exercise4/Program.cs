8eb3bf1e861db4805f6215c79739167c31252258
nastya <svistuna4444@gmail.com>
2023-04-24 14:28:59 +0300

    add home work exercise 4

---
+++ HomeWork_exercise4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
+Console.Write("введите целое число №1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите целое число №2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите целое число №3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max)
+    max = num2;
+else if (num3 > max)
+    max = num3;
+Console.WriteLine(max);
